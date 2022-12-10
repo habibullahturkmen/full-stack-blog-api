9c6db830a9dbf4bf32cb4ad322edaaa3bb2a03c8
habibullahturkmen <habibullah_turkmen@yahoo.com>
2022-12-10 21:40:56 +0430

    add post and update post functionality added

--- index.js
+++ index.js
@@ -3,12 +3,29 @@ import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/user.js";
 import postRoutes from "./routes/posts.js";
-import {Database} from "./db.js";
+import multer from "multer";
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "../client/public/upload");
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now()+file.originalname);
+    }
+})
+
+const upload = multer({ storage: storage })
+
+app.post("/api/upload", upload.single("file"), (req, res) => {
+    const file = req.file;
+    res.status(200).json(file.filename);
+});
+
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/posts", postRoutes);
